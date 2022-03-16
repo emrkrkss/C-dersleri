60dd748aa81efda0d4da1ec01c9f3d7615b52c73
emrkrkss <99718205+emrkrkss@users.noreply.github.com>
2022-03-16 08:39:32 +0300

    Add files via upload
    Yeni proje oluşturuldu.

---
+++ 02.03.22/Untitled1.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (void)
+{
+	int mesafe;
+	int hiz;
+	printf("Gidilecek Mesafe(KM) :");
+	scanf("%d", &mesafe);
+	printf("Arac Hizini Giriniz(km/s) :");
+	scanf("%d", &hiz);
+	
+	int toplam_saniye = (mesafe/ (float)hiz) * 3600;
+	printf("Gecirilecek Sure : %d saat, %d dakika, %d saniye. \n", toplam_saniye = toplam_saniye / 3600, (toplam_saniye % 3600) / 60, toplam_saniye % 60);
+}
