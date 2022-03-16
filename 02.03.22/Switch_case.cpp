f6dfa67dae420c1c0ca9c563189c1ea970232c16
emrkrkss <99718205+emrkrkss@users.noreply.github.com>
2022-03-16 09:05:00 +0300

    Yeni uygulama yazıldı.

---
+++ 02.03.22/Switch_case.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+main (){
+	int i;
+	printf("Rakam giriniz: ");
+	scanf("%d", &i);
+	switch(i){ 
+	
+		case 1 : printf("Bir");
+		break;
+		case 2 : printf("Ýki");
+		break;
+		case 3 : printf("uc");
+		break;
+		case 4 : printf("Dort");
+		break;
+		case 5 : printf("Bes");
+		break;
+		case 6 : printf("alti");
+		break;
+		case 7 : printf("Yedi");
+		break;
+		case 8 : printf("Sekiz");
+		break;
+		case 9 : printf("Dokuz");
+		break;
+		default : printf("Hicbiri");
+		
+	}
+}
