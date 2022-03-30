a1da6c7a4bf51434bee2fb7d7310eb33fe63f4ec
emrkrkss <99718205+emrkrkss@users.noreply.github.com>
2022-03-30 09:53:20 +0300

    Uygulama Hatası Giderildi.

--- 02.03.22/DoWhileÇarpmaAmaToplamaİle.cpp
+++ 02.03.22/DoWhileÇarpmaAmaToplamaİle.cpp
@@ -1,18 +1,19 @@
 #include<stdio.h>
 int main(){
-	int sayi1, sayi2;
-	int sonuc = 1;
-	//int adet = 0;
+	int sayi1 = 1, sayi2 = 1;
+	int sonuc = 0;
 	
+	printf("Sayi 1 Giriniz: ");
+	scanf("%d", &sayi1);
+	printf("Sayi 2 Giriniz: ");
+	scanf("%d", &sayi2);
 	do{
-	    printf("Sayi 1 Giriniz: ");
-	    scanf("%d", &sayi1);
-	    printf("Sayi 2 Giriniz: ");
-	    scanf("%d", &sayi2);
+	   
 	    
-		sonuc = sayi1 + sayi1;
+		sonuc = sonuc + sayi1;
+		sayi2--;
 		
 	}
-	while (sayi1<=sayi2);
+	while (sayi2>0);
 	printf("Sonuc = %d ", sonuc);
 }
