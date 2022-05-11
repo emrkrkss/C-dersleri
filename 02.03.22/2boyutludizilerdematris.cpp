3fc18622260054865ecf42a138ff297d46f457fb
emrkrkss <99718205+emrkrkss@users.noreply.github.com>
2022-05-11 10:19:26 +0300

    Hatalar giderildi.

--- 02.03.22/2boyutludizilerdematris.cpp
+++ 02.03.22/2boyutludizilerdematris.cpp
@@ -1,31 +1,30 @@
-#include<stdio.h>
-main(){
-	int notlar[3][5] =
-	{{20,60,90,70,10}, //Öðrenci1
-	{30,75,90,75,25},  //Öðrenci2
-	{50,80,90,45,30} //Öðrenci3
-	
-	};
-	for(int i=0; i<3;i++) //satýr tarama
-	{
-		for(int k=0; k<5; k++) //sütun tarama
+#include"stdio.h"
+main()
+{
+	int notlar[3][5]= {{20,60,90,70,10},    
+	                   {30,75,90,75,25},
+					   {50,80,90,45,30}};
+					    
+	for(int i=0;i<3;i++){
+		for(int k=0;k<5;k++)
 		{
-			printf("%3d", notlar[i][k]);
+			printf("%3d",notlar[i][k]);
 		}
 		printf("\n");
 	}
-	printf("\nFinal ortalamasi\n");
+	printf("\nFinal ortalamsi:\n");
 	float f_toplam=0;
-	for(int i=0; i<5; i=i+1) //2. satýrdakileri yani tüm Öðrencilerin Final notlarýnýn tamamýný alma
+	for(int i=0;i<5;i++)
 	{
-		f_toplam = notlar[2][i];
+		f_toplam+=notlar[2][i];
 	}
-	printf("Final ortalamasi = %f\n", (f_toplam/5.0));
-	float ucuncu_ogr_ort=0;
-	for(int i=0; i<3; i++)
-	{
-		ucuncu_ogr_ort +=notlar[i][3];
-		
-	}
-	printf("\n 3. Ýndexteki Ogrencinin Ortalamasý = %f\n", (ucuncu_ogr_ort/3.0));
+ printf("Final Ortalamsi=%f\n",(f_toplam/5.0));
+ float ucuncu_ogr_ort=0;
+ for(int i=0;i<3;i++)
+ {
+ 	ucuncu_ogr_ort+=notlar[i][3];
+ 	
+ }
+ printf("\n3. indexdeki ogr'nin ortalamsi = %f\n",(ucuncu_ogr_ort/3));
+ 
 }
