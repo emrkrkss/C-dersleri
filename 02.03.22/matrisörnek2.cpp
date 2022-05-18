54a3ea3d6a0019eb47b0453b3859d8b1701502ca
emrkrkss <99718205+emrkrkss@users.noreply.github.com>
2022-05-18 09:14:34 +0300

    Yeni uygulama yazıldı.

---
+++ 02.03.22/matrisörnek2.cpp
@@ -0,0 +1,18 @@
+#include"stdio.h"
+main()
+{
+	char a[5][5]= {' '};    
+	for(int i=0;i<5;i++){ //satýr sutun kontrolü
+		for(int k=0;k<5;k++)
+		{
+			if(i+k==2 || i+k==6)  
+			a[i][k]='*';
+			else if(i+k==4 && i%2==1)
+			a[i][k]='*';
+			printf("%c", a[i][k]);
+		}
+		printf("\n");
+	
+}
+}
+
